6241ca56bdc60cad66d0eb5750ba4a1f023bc406
so1omon <solmin3665@gmail.com>
2021-02-23 02:07:09 +0900

    version 1.0.1

--- Test.php
+++ Test.php
@@ -1,4 +1,13 @@
-<?php
-    print "My First PHP FILES!!<br/>";
-
-?>
+<!DOCTYPE html>
+<html lang="kor">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <title>MyPHP</title>
+    </head>
+    <body>
+        <h1>My First Page!!</h1>
+    </body>
+</html>
