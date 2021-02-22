0438875a8b15be67bd25eccd3c4d2e80a654b341
so1omon <solmin3665@gmail.com>
2021-02-23 02:11:38 +0900

    version 1.0.1.1

--- Test.php
+++ Test.php
@@ -6,8 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <title>MyPHP</title>
+        <style>
+        </style>
     </head>
     <body>
-        <h1>My First Page!!</h1>
+        <h1 style="color:red;">My First Page!!</h1>
     </body>
 </html>
